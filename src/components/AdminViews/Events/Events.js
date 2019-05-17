f6c6abbcb523c68430dc968a654cf7085a39f938
Joe Schlachtenhaufen <JDSchlach@gmail.com>
2019-05-17 15:32:06 -0500

    styling updated for cards

--- src/components/AdminViews/Events/Events.js
+++ src/components/AdminViews/Events/Events.js
@@ -10,13 +10,34 @@ class Events extends Component {
                     <div>
                         <h1>EVENTS</h1>
                     </div>
+                    <button className="midButton">NEW EVENT</button> 
+
                     <div className="eventDivs" >
-                        <p>CASE / CLIENT </p>
-                        <p>FUTURE DATE, PASS DATE?</p> 
-                        <p>EVENT:</p> 
-                        <p> ____ has a court date on ____ in downtown minneapolis at 1pm. </p>
-                        <p>where is the date? </p>
+                        <div className='cardHead'>
+                            <div className='cardHeadLeft'>
+                                <h1 className="eventh1">CLIENT NAME</h1>
+                            </div>
+                            <div className='cardHeadRight'>
+                                <p className="eventP">1-1-2019</p> 
+                            </div>
+                        </div>
+                        <h1 className="eventh1">EVENT:</h1>
+                        <p className="eventBody"> Client has a court date on 1-1-19 in downtown minneapolis at 1pm.</p>
                     </div>
+
+                    <div className="eventDivs" >
+                        <div className='cardHead'>
+                            <div className='cardHeadLeft'>
+                                <h1 className="eventh1">CLIENT NAME</h1>
+                            </div>
+                            <div className='cardHeadRight'>
+                                <p className="eventP">12-15-2018</p> 
+                            </div>
+                        </div>
+                        <h1 className="eventh1">EVENT:</h1>
+                        <p className="eventBody"> Client has a housing meeting in uptowntown minneapolis at 1pm.</p>
+                    </div>
+
                 </center>
             </div>
         );
