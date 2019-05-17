587579c5c9a5ed40c06aa8cc8dd686accea967e4
Kingman Douglass <kreedd88@gmail.com>
2019-05-17 14:17:13 -0500

    fixed bugs with converting junos docs

--- src/components/AdminViews/Cases/Cases.js
+++ src/components/AdminViews/Cases/Cases.js
@@ -5,21 +5,35 @@ class Cases extends Component {
     addCase = () => {
         this.props.history.push('/bio-form')
     }
+ 
+    viewCase = () => {
+        this.props.history.push('/case-list')
+    }
+ 
+    editCase = () => {
+        this.props.history.push('/edit-list')
+    }
 
     render() {
         return (
             <div>
-                <center>
-                    <div>
-                        <h1>CASES MANAGEMENT</h1>
-                    </div>
-                    <button className="adminMenuButtons">VIEW CASES</button> <br />
-                    <button className="adminMenuButtons"
-                    onClick={this.addCase}
-                    >ADD NEW CASE</button> <br/> 
+                 <center>
+                   <div>
+                       <h1>CASES MANAGEMENT</h1>
+                   </div>
 
-                    <button className="adminMenuButtons">EDIT EXISTING CASE</button> <br /> 
-                </center>
+                   <button className="adminMenuButtons"
+                    onClick={this.viewCase}
+                   >VIEW CASES</button> <br />
+
+                   <button className="adminMenuButtons"
+                   onClick={this.addCase}
+                   >ADD NEW CASE</button> <br/>
+
+                   <button className="adminMenuButtons"
+                   onClick={this.editCase}
+                   >EDIT EXISTING CASE</button> <br />
+               </center>
                 
             </div>
         );
