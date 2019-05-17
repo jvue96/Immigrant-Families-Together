3d435203dfd94c2db789bd4a86d7721692ac5d47
Kingman Douglass <kreedd88@gmail.com>
2019-05-17 13:05:31 -0500

    set up state and corrections on medical form

--- src/components/AdminViews/Cases/Forms/MedicalForm.js
+++ src/components/AdminViews/Cases/Forms/MedicalForm.js
@@ -8,6 +8,26 @@ class MedicalForm extends Component {
         this.props.history.push('/school-form')
     }
 
+    state = {
+        medicalForm: {
+            doctor_name: '',
+            doctor_phone: '',
+            medical_conditions: '',
+            counselor: '',
+            counselor_phone: '',
+            pediatrician: '',
+            pediatrician_phone: '',
+            optometrist: '',
+            optometrist_phone: '',
+            dentist: '',
+            dentist_phone: '',
+            vaccinations: '',
+            insurance_card_info: '',
+            fee_coverage: '',
+            medical_notes: ''
+        }
+    }
+    
     render() {
         return (
             <div>
@@ -18,23 +38,33 @@ class MedicalForm extends Component {
                         </h1>
                     </div>
                     <div className="formDivs">
-                        <label>PIMARY DOCTOR INFO</label> <br/>
+                        <label>PRIMARY DOCTOR NAME</label> <br/>
                         <input type="text" /> <br/>
-                        <label>KNOWN CONDITIONS</label> <br/>
+                        <label>PRIMARY DOCTOR PHONE</label> <br/>
                         <input type="text" /> <br/>
-                        <label>COUNSELOR INFO</label> <br/>
+                        <label>COUNSELOR NAME</label> <br/>
                         <input type="text" /> <br/>
-                        <label>PEDIATRICIAN INFO</label> <br/>
+                        <label>COUNSELOR PHONE</label> <br/>
                         <input type="text" /> <br/>
-                        <label>OPTOMATRIST INFO</label> <br/>
+                        <label>PEDIATRICIAN NAME</label> <br/>
                         <input type="text" /> <br/>
-                        <label>DENTIST INFO</label> <br/>
+                        <label>PEDIATRICIAN PHONE</label> <br/>
+                        <input type="text" /> <br/>
+                        <label>OPTOMETRIST NAME</label> <br/>
+                        <input type="text" /> <br/>
+                        <label>OPTOMETRIST PHONE</label> <br/>
+                        <input type="text" /> <br/>
+                        <label>DENTIST NAME</label> <br/>
+                        <input type="text" /> <br/>
+                        <label>DENTIST PHONE</label> <br/>
                         <input type="text" /> <br/>
                         <label>VACCINATIONS</label> <br/>
                         <input type="text" /> <br/>
-                        <label>INSURANCE</label> <br/>
+                        <label>INSRUANCE CARD INFO</label> <br/>
                         <input type="text" /> <br/>
                         <label>FEE COVERAGE</label> <br/>
+                        <input type="text" /> <br/>
+                        <label>MEDICAL NOTES</label> <br/>
                         <input type="text" /> <br/>
                         <button
                         onClick={this.next}
