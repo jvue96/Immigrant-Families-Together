8920f4e4fd0993b948c4011a5104441d0cf7e976
Kingman Douglass <kreedd88@gmail.com>
2019-05-17 21:04:47 -0500

    forgot to branch, uncommented code and switched databse name

--- src/redux/sagas/medicalSaga.js
+++ src/redux/sagas/medicalSaga.js
@@ -8,8 +8,8 @@ function* postMedical(action) {
     try {
         console.log('POST medical form', action);
         const getResponse = yield axios.post(`/api/forms/medical`, action.payload);
-        const action = {type: 'SET_MEDICAL', payload: getResponse.data};
-        yield put(action);
+        // const action = {type: 'SET_MEDICAL', payload: getResponse.data};
+        // yield put(action);
     }catch (error) {
         console.log(`Couldn't POST the medical form`);
         alert(`Sorry couldn't add the current submission. Try again later.`)
