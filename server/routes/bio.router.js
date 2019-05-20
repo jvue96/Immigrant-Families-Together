c97e4319874f51ead10d4edef9d8f8431879cd15
BENJAMIN RAGSDALE <benjaminragsdale@BENJAMINs-MacBook-Pro.local>
2019-05-20 15:39:32 -0500

    bio.router.js sqltext added

--- server/routes/bio.router.js
+++ server/routes/bio.router.js
@@ -3,11 +3,11 @@ const pool = require('../modules/pool');
 const router = express.Router();
 
 router.post('/', (req, res) => {
-    let aid = req.body;
-    console.log('Adding in aid:', aid);
-    let sqlText = `INSERT INTO bio (grocery_program, grocery_program_volunteer, go_fund_me, social_worker, social_worker_phone) VALUES 
-    ($1, $2, $3, $4, $5)`;
-    pool.query(sqlText, [aid.grocery_program, aid.grocery_program_volunteer, aid.go_fund_me, aid.social_worker, aid.social_worker_phone])
+    let bio = req.body;
+    console.log('Adding in primary individual:', bio);
+    let sqlText = `INSERT INTO primary_indvidual (last_name, first_name, dob, spouse_first_name, spouse_dob, phone, email, address, utilities, referred_by, reference_date, passport, us_id) VALUES 
+    ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13)`;
+    pool.query(sqlText, [bio.last_name, bio.first_name, bio.dob, bio.spouse_first_name, bio.spouse_dob, bio.phone, bio.email, bio.address, bio.utilities, bio.referred_by, bio.reference_date, bio.passport, bio.us_id])
       .then( (response) => {
         res.sendStatus(201);
       })
