4ba46c0aa2c4779864f09d86dd6a01e17af3190c
Juno Vue <Juno.Vue@Hotmail.com>
2019-05-29 11:00:55 -0500

    conditional rendering for team

--- src/components/VolunteerViews/Team/Team.js
+++ src/components/VolunteerViews/Team/Team.js
@@ -13,40 +13,69 @@ class Team extends Component {
     }
 
     render() {
+
+        let emptyTeam; 
+        if(this.props.reduxState.teamReducer.length === 0) {
+            emptyTeam = <div className="bioCard">
+                            <hr/>
+                            <label>NAME</label> 
+                            <p className="PCard"></p>
+                            <hr/>
+                            <label>PHONE</label> 
+                            <p className="PCard"></p>
+                            <hr/>
+                            <label>ENCRYPTED</label> 
+                            <p className="PCard"></p>
+                            <hr/>
+                            <label>EMAIL</label> 
+                            <p className="PCard"></p>
+                            <hr/>
+                            <label>SKILLS</label> 
+                            <p className="PCard"></p>
+                            <hr/>
+                            <label>SECOND LANGUAGE</label> 
+                            <p className="PCard"></p>
+                            <hr/>
+                            <label>ADDRESS</label> 
+                            <p className="PCard"></p>
+                            <hr/>
+                        </div>
+                    }
+
         return (
             <div>
- <Nav pageName='TEAM' volunteer home='/home' /> 
-            <center> 
-                <h1> VOLUNTEERS </h1>
-                {this.props.reduxState.teamReducer.map(team => (
-                <div className="bioCard">
-                    <hr/>
-                    <label>NAME</label> 
-                    <p className="PCard">{team.username}</p>
-                    <hr/>
-                    <label>PHONE</label> 
-                    <p className="PCard">{team.phone}</p>
-                    <hr/>
-                    <label>ENCRYPTED</label> 
-                    <p className="PCard">{team.encrypted}</p>
-                    <hr/>
-                    <label>EMAIL</label> 
-                    <p className="PCard">{team.email}</p>
-                    <hr/>
-                    {/* <label>ROLE</label> <span> </span> <br/> */}
-                    <label>SKILLS</label> 
-                    <p className="PCard">{team.skills}</p>
-                    <hr/>
-                    <label>SECOND LANGUAGE</label> 
-                    <p className="PCard">{team.second_language}</p>
-                    <hr/>
-                    <label>ADDRESS</label> 
-                    <p className="PCard">{team.address}</p>
-                    <hr/>
+                <Nav pageName='TEAM' volunteer home='/home' /> 
+                    <center> 
+                        <h1> VOLUNTEERS </h1>
+                        {emptyTeam}
+                        {this.props.reduxState.teamReducer.map((team,index) => (
+                            <div className="bioCard" key={index}>
+                                <hr/>
+                                <label>NAME</label> 
+                                <p className="PCard">{team.username}</p>
+                                <hr/>
+                                <label>PHONE</label> 
+                                <p className="PCard">{team.phone}</p>
+                                <hr/>
+                                <label>ENCRYPTED</label> 
+                                <p className="PCard">{team.encrypted}</p>
+                                <hr/>
+                                <label>EMAIL</label> 
+                                <p className="PCard">{team.email}</p>
+                                <hr/>
+                                <label>SKILLS</label> 
+                                <p className="PCard">{team.skills}</p>
+                                <hr/>
+                                <label>SECOND LANGUAGE</label> 
+                                <p className="PCard">{team.second_language}</p>
+                                <hr/>
+                                <label>ADDRESS</label> 
+                                <p className="PCard">{team.address}</p>
+                                <hr/>
+                            </div>
+                        ))}
+                    </center>
                 </div>
-                  ))}
-            </center>
-            </div>
 
         );
     }
